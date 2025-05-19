5d981e9c58cf1520ac9b4d1f537598a11421dcc7
Kamilean <94792399+Kamilean@users.noreply.github.com>
2025-05-19 14:16:59 +0900

    Update 4_2_star_ans.cs

--- assignment4/4_2_star_ans.cs
+++ assignment4/4_2_star_ans.cs
@@ -11,7 +11,37 @@ namespace star
             int size = 2 * (radius + 1);
 
             // ---------- TODO ----------
-
+            for (int i=0;i<size;i++)
+            {
+                string line ="";
+                for (int j=0;j<size;j++)
+                {
+                    if (i==0||i==size-1)
+                    {
+                        if (j<size*2/3)
+                            line += "*";
+                        else
+                            line += " ";
+                    }
+                    else if (j==0)
+                    {
+                        line+= "*";
+                    }
+                    else
+                    {
+                        line += " ";
+                    }
+                }
+                line += "    ";
+                for (int j=0;j<size;j++)
+                {
+                    if (i==size/3||i==2*size/3||j==size/3||j==2*size/3)
+                        line += "*";
+                    else
+                        line += " ";
+                }
+                Console.WriteLine(line);
+            }
             // --------------------
         }
 
